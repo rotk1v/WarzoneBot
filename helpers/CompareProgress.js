837aa22dcda5ca3d2e5883498e81440f7c6e88c6
Viktor <toffen2011@gmail.com>
2021-06-21 04:27:57 +0200

    adjustments to undefined comparison check

--- helpers/CompareProgress.js
+++ helpers/CompareProgress.js
@@ -1,16 +1,18 @@
 module.exports = (old, fresh, p, f) => {
   if (p) {
+    if (old == undefined || fresh == undefined) return "";
     if (old < fresh) return `[+${(fresh - old).toFixed(3)}%]`;
     if (old > fresh) return `[${(fresh - old).toFixed(3)}%]`;
     if (old == fresh) return "";
   }
   if (f) {
+    if (old == undefined || fresh == undefined) return "";
     if (old < fresh) return `[+${(fresh - old).toFixed(3)}]`;
     if (old > fresh) return `[${(fresh - old).toFixed(3)}]`;
     if (old == fresh) return "";
   }
+  if (old == undefined || fresh == undefined) return "";
   if (old < fresh) return `[+${fresh - old}]`;
   if (old > fresh) return `[${fresh - old}]`;
   if (old == fresh) return "";
-  if (old == undefined || fresh == undefined) return "";
 };
