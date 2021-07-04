7de2328009112a19367fe8dc63bf3f333fbb7ce6
Viktor <toffen2011@gmail.com>
2021-07-04 18:12:20 +0200

    checks for changed amount

--- helpers/CompareProgress.js
+++ helpers/CompareProgress.js
@@ -1,18 +1,30 @@
 module.exports = (old, fresh, p, f) => {
   if (p) {
     if (!old || !fresh) return "";
-    if (old < fresh) return `[+${(fresh - old).toFixed(3)}%]`;
-    if (old > fresh) return `[${(fresh - old).toFixed(3)}%]`;
+    if (old < fresh && fresh - old > parseFloat("0.0009")) {
+      return `[+${(fresh - old).toFixed(3)}%]`;
+    }
+    if (old > fresh && fresh - old > parseFloat("0.0009")) {
+      console.log(fresh - old);
+      return `[${(fresh - old).toFixed(3)}%]`;
+    }
     if (old == fresh) return "";
+    return "";
   }
   if (f) {
     if (old == undefined || fresh == undefined) return "";
-    if (old < fresh) return `[+${(fresh - old).toFixed(3)}]`;
-    if (old > fresh) return `[${(fresh - old).toFixed(3)}]`;
+    if (old < fresh && fresh - old > parseFloat("0.0009"))
+      return `[+${(fresh - old).toFixed(3)}]`;
+    if (old > fresh && fresh - old > parseFloat("0.0009"))
+      return `[${(fresh - old).toFixed(3)}]`;
     if (old == fresh) return "";
+    return "";
   }
   if (old == undefined || fresh == undefined) return "";
-  if (old < fresh) return `[+${fresh - old}]`;
-  if (old > fresh) return `[${fresh - old}]`;
+  if (old < fresh && fresh - old > parseFloat("0.0009"))
+    return `[+${fresh - old}]`;
+  if (old > fresh && fresh - old > parseFloat("0.0009"))
+    return `[${fresh - old}]`;
   if (old == fresh) return "";
+  return "";
 };
